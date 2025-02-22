e6f24c66b2f83a46952784a23cf5627e2f447969
marwan <marwanossama2004@gmail.com>
2025-02-22 22:48:13 +0200

    before LINQ - Aggregate Operators

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,27 @@
 ﻿using static LinqDemo.ListGenerator;
 using LinqDemo;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+using Microsoft.VisualBasic;
 namespace ConsoleApp1
 {
+    
+
+    #region 2. Uses a custom comparer to do a case-insensitive sort of the words in an array.
+    class CaseInsensitiveComparer : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            x.ToLower();
+            y.ToLower();
+
+            return x.CompareTo(y);
+        }
+    }
+    #endregion
+
+    
+
     internal class Program
     {
         static void Main(string[] args)
@@ -97,6 +117,102 @@ namespace ConsoleApp1
             //var res = lines.Select(w => w.Length).Average();
             //Console.WriteLine(res); 
             #endregion
+
+            #endregion
+
+            #region LINQ - Ordering Operators
+            #region 1. Sort a list of products by name
+            //var res = ProductList.OrderBy(p => p.ProductName);
+            //foreach(var item in res) Console.WriteLine(item);
+            #endregion
+
+            #region 2. Uses a custom comparer to do a case-insensitive sort of the words in an array.
+            //String[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+            //Array.Sort(Arr, new CaseInsensitiveComparer());
+            //foreach(var item in Arr) Console.WriteLine(item); 
+            #endregion
+
+            #region 3. Sort a list of products by units in stock from highest to lowest.
+            //var result = ProductList.OrderByDescending(p => p.UnitsInStock);
+            //foreach (var item in result) Console.WriteLine(item);
+            #endregion
+
+            #region 4. Sort a list of digits, first by length of their name, and then alphabetically by the name itself
+            //string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            //var res = Arr.OrderBy(x => x.Length).ThenBy(x => x);
+            //foreach(var item in res) Console.WriteLine(item);
+            #endregion
+
+            #region 5. Sort first by-word length and then by a case-insensitive sort of the words in an array.
+            //String[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+            //var res = Arr.OrderBy(x => x.Length).ThenBy(x => x, new CaseInsensitiveComparer());
+            //foreach(var item in res) Console.WriteLine(item);
+            #endregion
+
+            #region 6. Sort a list of products, first by category, and then by unit price, from highest to lowest.
+            //var res = ProductList.OrderBy(p => p.Category).ThenByDescending(p => p.UnitPrice);
+            //foreach(var item in res) Console.WriteLine(item);
+            #endregion
+
+            #region 7. Sort first by-word length and then by a case-insensitive descending sort of the words in an array.
+            //String[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+            //var res = Arr.OrderBy(x => x.Length).ThenBy(x => x, new CaseInsensitiveComparer());
+            //foreach(var item in res) Console.WriteLine(item); 
+            #endregion
+
+            #region 8. Create a list of all digits in the array whose second letter is 'i' that is reversed from the order in the original array.
+            //String[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            //var res = Arr.Where(x => x[1] == 'i').Reverse();
+            //foreach(var item in res) Console.WriteLine(item); 
+            #endregion
+
+            #endregion
+
+            #region LINQ – Transformation Operators
+
+            #region 1. Return a sequence of just the names of a list of products.
+            //var res = ProductList.Select(p => p.ProductName).ToList();
+            //foreach(var item in res) Console.WriteLine(item); 
+            #endregion
+            #region 2. Produce a sequence of the uppercase and lowercase versions of each word in the original array (Anonymous Types)
+
+            //String[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+            //var res = words.Select(w => new { lower = w.ToLower(), upper = w.ToUpper() });
+            //foreach(var item in res) Console.WriteLine(item); 
+            #endregion
+            #region 3. Produce a sequence containing some properties of Products, including UnitPrice which is renamed to Price in the resulting type.
+            //var res = ProductList.Select(p => new { name = p.ProductName, price = p.UnitPrice });
+            //foreach(var item in res) Console.WriteLine(item); 
+            #endregion
+
+            #region 4. Determine if the value of int in an array matches their position in the array.
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var res = Arr.Select(x => $"{x}: {x == Arr[x]}");
+            //foreach(var item in res) Console.WriteLine(item); 
+            #endregion
+
+            #region 5. Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            //var res = from a in numbersA
+            //          from b in numbersB
+            //          where a < b
+            //          select $"{a} is less that {b}";
+
+            //foreach(var item in res) Console.WriteLine(item); 
+            #endregion
+
+            #region 6. Select all orders where the order total is less than 500.00.
+            //var res = CustomerList.SelectMany(c => c.Orders).Where(o => o.Total < 500);
+            //foreach(var item in res) Console.WriteLine(item); 
+            #endregion
+
+            #region 7. Select all orders where the order was made in 1998 or later.
+            //var res = CustomerList.SelectMany(c => c.Orders).Where(o => o.OrderDate.Year < 1998);
+            //foreach (var item in res) Console.WriteLine(item); 
+            #endregion
+
 
             #endregion
         }
